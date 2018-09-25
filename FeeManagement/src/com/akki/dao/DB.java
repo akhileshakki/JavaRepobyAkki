adfc682d500b92e0502c07ed27c40ee24fbabcbd
Akki <Akki@Akki>
2018-09-26 01:59:28 +0530

    # WARNING:Project Added

---
+++ FeeManagement/src/com/akki/dao/DB.java
@@ -0,0 +1,15 @@
+package com.akki.dao;
+
+import java.sql.*;
+public class DB {
+public static Connection getCon(){
+	Connection con=null;
+	try{
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:akki","system","manager");
+		}catch(Exception ex){
+		System.out.println(ex);
+	}
+	return con;
+ }
+}
